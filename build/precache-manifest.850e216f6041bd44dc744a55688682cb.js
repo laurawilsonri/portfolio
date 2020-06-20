4f550d77061768390980e300a9a794e8db5ab08b
laurawilsonri <laura_wilson@brown.edu>
2020-06-20 14:44:07 -0400

    deployed to heroku

---
+++ build/precache-manifest.850e216f6041bd44dc744a55688682cb.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa61776eb87ac4d0f8e885dd37099f5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec41e52498b72f49213a",
+    "url": "/static/css/main.1a8bf4c0.chunk.css"
+  },
+  {
+    "revision": "b78085b541059ccf4b5f",
+    "url": "/static/js/2.344e1100.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.344e1100.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec41e52498b72f49213a",
+    "url": "/static/js/main.7a5e4828.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "59f85adf6368a4c3c5f5c3eae86e8045",
+    "url": "/static/media/RESUME.59f85adf.pdf"
+  },
+  {
+    "revision": "c9bf2293116995ab2a0de36d69b9a5f6",
+    "url": "/static/media/artsy_profile.c9bf2293.png"
+  },
+  {
+    "revision": "479e4f075f4233530f45060f43d7b3c2",
+    "url": "/static/media/close-up.479e4f07.png"
+  },
+  {
+    "revision": "f4b9d0dab0cf8cec8d8ebd1bd707dbd9",
+    "url": "/static/media/dash_thumbnail.f4b9d0da.png"
+  },
+  {
+    "revision": "ac78cdc20b3d208fcd91915b890174cc",
+    "url": "/static/media/dv_home_page.ac78cdc2.png"
+  },
+  {
+    "revision": "c61b2158775b126a218265d571e58a70",
+    "url": "/static/media/ideation.c61b2158.png"
+  },
+  {
+    "revision": "12c700a87c24e82371c5427052c86952",
+    "url": "/static/media/interaction_table.12c700a8.png"
+  },
+  {
+    "revision": "8cebe69ac6f8755208c6c467326c6184",
+    "url": "/static/media/jam_thumbnail.8cebe69a.png"
+  },
+  {
+    "revision": "f4a5c2e9f8d63676c8850e468235a0ab",
+    "url": "/static/media/netflix_actor_flow_with_tooltip.f4a5c2e9.png"
+  },
+  {
+    "revision": "546af466e54b113e0b5254f2bbecb9ec",
+    "url": "/static/media/netflix_runtime_graph.546af466.png"
+  },
+  {
+    "revision": "b731ff8b676c96b8d9346c8717050da9",
+    "url": "/static/media/netflix_thumbnail.b731ff8b.png"
+  },
+  {
+    "revision": "8f4acfb1f237b23e65334951ec87c706",
+    "url": "/static/media/orange_poster.8f4acfb1.png"
+  },
+  {
+    "revision": "ab2c00ade117b0253405badee92b3a71",
+    "url": "/static/media/poster_demo.ab2c00ad.mp4"
+  },
+  {
+    "revision": "760a58b8e1be7d602c7b33efa800e9a6",
+    "url": "/static/media/poster_thumbnail.760a58b8.png"
+  },
+  {
+    "revision": "ff7bf2c0ed849ec4b2721b66a8d3a269",
+    "url": "/static/media/thumbnail_dv.ff7bf2c0.png"
+  },
+  {
+    "revision": "70e3debe2c1467bdf443eee493f33cad",
+    "url": "/static/media/touch_menu_cropped.70e3debe.mp4"
+  },
+  {
+    "revision": "d6395f196228fb77fd56d9c66e4f774b",
+    "url": "/static/media/train_app_walkthrough_cropped.d6395f19.mp4"
+  },
+  {
+    "revision": "d2f7923b2bb76c6801ab7243242024cd",
+    "url": "/static/media/train_phone1.d2f7923b.png"
+  },
+  {
+    "revision": "02d751394aa6a7cbf9df253297da48b2",
+    "url": "/static/media/train_phone2.02d75139.png"
+  },
+  {
+    "revision": "d8d47bbf3b8fe9588bed0b81528bd6ba",
+    "url": "/static/media/train_physical.d8d47bbf.jpg"
+  },
+  {
+    "revision": "48cd960474d88cad92e8431af9b29790",
+    "url": "/static/media/train_physical_demo.48cd9604.mp4"
+  },
+  {
+    "revision": "199250ef0e23ecac927e219535f650ad",
+    "url": "/static/media/train_thumbnail.199250ef.jpg"
+  },
+  {
+    "revision": "c2253d28af652114b30c521a0cdd2ca6",
+    "url": "/static/media/translation_demo.c2253d28.mp4"
+  },
+  {
+    "revision": "4a746c1a1fa6c415e495c5290b693443",
+    "url": "/static/media/translation_diagram.4a746c1a.png"
+  },
+  {
+    "revision": "2bd1b7fef68b15f4faa970f2aacc8f24",
+    "url": "/static/media/translation_thumbnail.2bd1b7fe.png"
+  },
+  {
+    "revision": "779a1206ecc842e34974f09dffddad58",
+    "url": "/static/media/zoomed-out.779a1206.png"
+  }
+]);
